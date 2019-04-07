9633a4f0f7a7ed5fcb1860eaa6a22516b9b129fe
ewilson87 <32503240+ewilson87@users.noreply.github.com>
2019-04-07 16:36:19 -0700

    Delete securePage.php

--- CST236_MainProject/securePage.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include_once 'header.php';
-
-if (isset($_SESSION['principle']) == false || $_SESSION[principle] == null || $_SESSION['principle'] == false) {
-    header("Location: login.html");
-}
